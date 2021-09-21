6194618d805cf650327ba91fdf8453aba2c78d80
Lara W Weed <laraweed@sh02-ln02.stanford.edu>
2021-09-21 11:39:24 -0700

    Scripts from Renske & Dwijen

---
+++ 3_nparACT.R
@@ -0,0 +1,41 @@
+library("nparACT")
+
+imputed = "D:/UKBiobank/Data/biobankAccelerometerAnalysis/Files"
+
+# IMPUTED
+
+files <- list.files(path=imputed,
+                    pattern="*.csv", full.names=TRUE, recursive=FALSE)
+
+all_data <- data.frame()
+
+counter <- 0
+
+pb <- progress::progress_bar$new(
+  format = " running imputed ivis [:bar] :percent eta: :eta",
+  total = length(files), clear = FALSE, width= 60)
+
+
+for (i in files){
+  pb$tick()
+  counter <- counter + 1
+  print(i)
+  fullfilename <- read.csv(i)
+  filename <- strsplit(i, split = "/")
+  filename <- filename[[1]][length(filename[[1]])]
+  
+  if (grepl("ZERODIV", filename)) {
+    all_data <- rbind(all_data, cbind(filename = filename, setNames(data.frame(matrix(ncol = 7, nrow = 1)), c("IS", "IV", "RA", "L5", "L5_starttime", "M10", "M10_starttime"))))
+  } else {
+    single_result <- nparACT_base("fullfilename", SR = 2/60, plot = T)
+    print(single_result)
+    single_result <- cbind(filename = filename, single_result)
+    all_data <- rbind(all_data, single_result)
+  }
+  
+}
+
+print(all_data)
+setwd("D:/UKBiobank/Data/biobankAccelerometerAnalysis/Files")
+write.csv(all_data, "IVIS_data_ORIGINAL.csv")
+
---
+++ Dwijen.py
@@ -0,0 +1,7 @@
+from accelerometer import accUtils
+accUtils.writeStudyAccProcessCmds(
+"/mnt/d/UKBiobank/Data/Scripts/cwa",
+outDir="/mnt/d/UKBiobank/Data/Scripts/out",
+cmdsFile="process-cmds.txt"
+)
+
