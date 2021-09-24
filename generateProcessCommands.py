c52c71be317b8005f62d2c3d80d6c5c347a04acb
unknown <larawweed@gmail.com>
2021-09-23 18:42:59 -0700

    rename Dwijen.py to generateProcessCommands.py and start writing batch script

---
+++ generateProcessCommands.py
@@ -0,0 +1,12 @@
+import datetime
+import sys
+sys.path.append("/scratch/groups/jzeitzer/UKBB/Code/biobankAccelerometerAnalysis/")
+
+
+from accelerometer import accUtils
+accUtils.writeStudyAccProcessCmds(
+"/scratch/groups/jzeitzer/UKBB/Data/Raw/Batch_test/",
+outDir="/scratch/groups/jzeitzer/UKBB/Data/Outputs/Batch_test/",
+cmdsFile="processCmds.txt"
+)
+
