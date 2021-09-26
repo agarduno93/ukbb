e5f2c9b3de41c19d63195cdd2d7f184882fae707
Lara W Weed <laraweed@stanford.edu>
2021-09-26 11:51:04 -0700

    Input chunk into 10 files due to UKBB constraints

--- generateProcessCommands.py
+++ generateProcessCommands.py
@@ -5,8 +5,8 @@ sys.path.append("/scratch/groups/jzeitzer/UKBB/Code/biobankAccelerometerAnalysis
 
 from accelerometer import accUtils
 accUtils.writeStudyAccProcessCmds(
-"/scratch/groups/jzeitzer/UKBB/Data/Raw/Batch_test/",
-outDir="/scratch/groups/jzeitzer/UKBB/Data/Outputs/Batch_test/",
+"/scratch/groups/jzeitzer/UKBB/Data/Raw/Batch_fullfile/",
+outDir="/scratch/groups/jzeitzer/UKBB/Data/Outputs/Batch_fullfile/",
 cmdsFile="processCmds.txt"
 )
 
