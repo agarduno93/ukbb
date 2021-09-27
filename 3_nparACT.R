d3211e21ac5834884492f90ec51cdae63b94471d
Lara-Weed <laraweed@stanford.edu>
2021-09-26 23:33:49 -0700

    CWA2CSV pipeline development

--- 3_nparACT.R
+++ 3_nparACT.R
@@ -1,5 +1,6 @@
+rm(list=ls())
 library("nparACT")
-imputed = "/scratch/groups/jzeitzer/UKBB/Data/CSV/Batch_test"
+imputed <- Sys.getenv("DATACSV")
 
 files <- list.files(path=imputed,
                     pattern="*.csv", full.names=TRUE, recursive=FALSE)
@@ -9,21 +10,23 @@ all_data <- data.frame()
 counter <- 0
 
 for (i in files){
+  tryCatch({
   counter <- counter + 1
   print(i)
   fullfilename <- read.csv(i)
+  fullfilename<- subset(fullfilename, select=c("time", "acc"))
   filename <- strsplit(i, split = "/")
   filename <- filename[[1]][length(filename[[1]])]
   
   if (grepl("ZERODIV", filename)) {
     all_data <- rbind(all_data, cbind(filename = filename, setNames(data.frame(matrix(ncol = 7, nrow = 1)), c("IS", "IV", "RA", "L5", "L5_starttime", "M10", "M10_starttime"))))
   } else {
-    single_result <- nparACT_base("fullfilename", SR = 2/60, plot = T)
+    single_result <- nparACT_base("fullfilename", SR = 2/60, plot = F)
     print(single_result)
     single_result <- cbind(filename = filename, single_result)
     all_data <- rbind(all_data, single_result)
   }
-  
+  },error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
 }
 
 setwd("/scratch/groups/jzeitzer/UKBB/Data/IVIS")
--- generateProcessCommands.py
+++ generateProcessCommands.py
@@ -3,8 +3,11 @@ import sys
 import os
 
 path_baa = os.getenv('CODEBAA')
+print(path_baa)
 path_raw = os.getenv('DATARAW')
+print(path_raw)
 path_out = os.getenv('DATAOUTPUTS')
+print(path_out)
 
 sys.path.append(path_baa)
 
