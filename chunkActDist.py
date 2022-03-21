029b9b08ce222b300c7865ef3cb1a90fde1906ec
Lara-Weed <laraweed@stanford.edu>
2022-03-21 12:48:19 -0700

    Update to chunking for overall activity distribution

--- chunkActDist.py
+++ chunkActDist.py
@@ -12,29 +12,36 @@ csv_fn = os.listdir(path_csv)
 actdist_fn = os.listdir(path_szn)
 
 csv = [file[:-15] for file in csv_fn if file[-4:]=='.csv']
-actdist = [file[:-12] for file in actdist_fn if file[-4:]=='.csv']
+#actdist = [file[:-12] for file in actdist_fn if file[-4:]=='.csv']
 
-new_cmds = [line + "-timeSeries.csv" for line in csv if line not in actdist]
+new_cmds = [line + "-timeSeries.csv" for line in csv]# if line not in actdist]
 
 numLines = len(new_cmds)
 print(numLines)
-if numLines > 1:
-    if numLines < numFiles:
-        numFiles = numLines
-    chunkLines = math.ceil(numLines/numFiles)
-    if chunkLines>1:
-        for i in range (numFiles):
-            if i == 0:
-                ln = new_cmds[i*chunkLines:(i+1)*chunkLines]
-            else:
-                ln = new_cmds[(i*chunkLines)+1:(i+1)*chunkLines]
-            filename = path_comm +'/inputActDist_' + str(i) + '.txt'
-            f = open(filename, 'w')
-            f.writelines( "%s\n" % item for item in ln )
-            f.close()
-    else:            
-        ln = new_cmds
-        filename = path_comm +'/inputActDist_1.txt'
-        f = open(filename, 'w')
-        f.writelines( "%s\n" % item for item in ln )
-        f.close()
+
+ln = new_cmds
+filename = path_comm +'/inputActDist_1.txt'
+f = open(filename, 'w')
+f.writelines( "%s\n" % item for item in ln )
+f.close()
+
+#if numLines > 1:
+#    if numLines < numFiles:
+#        numFiles = numLines
+#    chunkLines = math.ceil(numLines/numFiles)
+#    if chunkLines>1:
+#        for i in range (numFiles):
+#            if i == 0:
+#                ln = new_cmds[i*chunkLines:(i+1)*chunkLines]
+#            else:
+#                ln = new_cmds[(i*chunkLines)+1:(i+1)*chunkLines]
+#            filename = path_comm +'/inputActDist_' + str(i) + '.txt'
+#            f = open(filename, 'w')
+#            f.writelines( "%s\n" % item for item in ln )
+#            f.close()
+#    else:            
+#        ln = new_cmds
+#        filename = path_comm +'/inputActDist_1.txt'
+#        f = open(filename, 'w')
+#        f.writelines( "%s\n" % item for item in ln )
+#        f.close()
